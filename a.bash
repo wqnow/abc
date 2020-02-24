a82c69f1e3d85d426d945afb687c7c29e66ef76b
Wang Qi <85624226@qq.com>
2020-02-24 22:16:26 +0800

    first run

---
+++ a.bash
@@ -0,0 +1,14 @@
+gdown --id 1QB1cw91ZMv2rzWSUYhtiQcHNzCWBPT3t
+unzip -o wq20200110.zip > /dev/null
+
+gdown --id 1ZpKJMsWfWPALcM37vIznmHSfdzD8CTPm
+mv *.gz networks
+
+chmod a+x autogtp
+chmod a+x leelaz
+
+for i in $(seq 1 10)
+do 
+./autogtp  -k ./train/ -m 10| grep minute
+sleep 23
+done
